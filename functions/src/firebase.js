fbd98e9d4572563ffc22ef9dd16ec69c62f21ce3
kihikojay <mwngjay@gmail.com>
2026-02-06 02:39:05 +0300

    api update Commit

--- functions/src/firebase.js
+++ functions/src/firebase.js
@@ -6,15 +6,6 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB7_OSRnhXPuAnCXPSgCm4bfLk0rTdouf8",
-  authDomain: "docfinder-49324.firebaseapp.com",
-  projectId: "docfinder-49324",
-  storageBucket: "docfinder-49324.firebasestorage.app",
-  messagingSenderId: "552233764986",
-  appId: "1:552233764986:web:b5d591212fed723cd753c3",
-  measurementId: "G-5816X0NG4X"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
